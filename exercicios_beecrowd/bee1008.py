c1d79f0b99415d8ca286eb32e312cd42462229d1
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-03-24 18:20:46 -0300

    Exercício Resolvido!

---
+++ exercicios_beecrowd/bee1008.py
@@ -0,0 +1,8 @@
+funcionario = int(input(''))
+horas = int(input(''))
+valor_hora = float(input(''))
+
+salario = horas * valor_hora
+
+print(f'NUMBER = {funcionario}')
+print(f'SALARY = U$ {salario:.2f}')
