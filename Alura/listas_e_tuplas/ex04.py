17de9925bebeac11a772acbba080cf683eac1f8a
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-05-05 14:03:03 -0300

    correção

--- Alura/listas_e_tuplas/ex04.py
+++ Alura/listas_e_tuplas/ex04.py
@@ -1,5 +1,5 @@
-estoque1 = tuple(input('Produtos do estoque 1 (separados por vírgula): ')).split()
-estoque2 = tuple(input('Produtos do estoque 2 (separados por vírgula): ')).split()
+estoque1 = tuple(input('Produtos do estoque 1 (separados por vírgula): ').split())
+estoque2 = tuple(input('Produtos do estoque 2 (separados por vírgula): ').split())
 
 estoque_combinado = estoque1 + estoque2
 
